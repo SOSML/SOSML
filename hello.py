d5086e6681a1ff553ebbd1a501e99a26d48456d3
Julian Baldus <s9jebald@stud.uni-saarland.de>
2017-05-17 17:06:17 +0200

    test

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
 from itertools import chain
 
+def range2(i, j):
+    result = []
+    while i < j:
+        result.append(i)
+        i += 1
+    return result
+
+tmp = range2(1, 1000000)
+
 class SolarSystem:
     planets = [list (chain (planet, (index + 1,))) for index, planet in enumerate ((
         ('Mercury', 'hot', 2240),
@@ -9,27 +18,22 @@ class SolarSystem:
         ('Jupiter', 'stormy', 71492),
         ('Saturn', 'ringed', 60268),
         ('Uranus', 'cold', 25559),
-        ('Neptune', 'very cold', 24766) 
+        ('Neptune', 'very cold', 24766)
     ))]
-    
+
     lines = (
         '{} is a {} planet',
         'The radius of {} is {} km',
         '{} is planet nr. {} counting from the sun'
     )
-    
+
     def __init__ (self):
         self.lineIndex = 0
-    
+
     def greet (self):
-        self.planet = self.planets [int (Math.random () * len (self.planets))]
-        document.getElementById ('greet') .innerHTML = 'Hello {}'.format (self.planet [0])
-        self.explain ()
-        
-    def explain (self):
-        document.getElementById ('explain').innerHTML = (
-            self.lines [self.lineIndex] .format (self.planet [0], self.planet [self.lineIndex + 1])
-        )
-        self.lineIndex = (self.lineIndex + 1) % 3
-        
+        return tmp
+
+    def explain (self, x):
+        tmp = x
+
 solarSystem = SolarSystem ()
