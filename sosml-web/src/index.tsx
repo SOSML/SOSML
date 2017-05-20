3e9f290c4ce4368aa121a49b4a7ffce633312fa7
Marvin Hofmann <marvin@marv-productions.de>
2017-05-20 16:04:18 +0200

    add component and tests

--- sosml-web/src/index.tsx
+++ sosml-web/src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import Hello from "./components/Hello";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Hello name="TypeScript" enthusiasmLevel={10}/>,
+    document.getElementById('root') as HTMLElement
 );
