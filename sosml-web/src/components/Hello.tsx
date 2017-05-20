14476de2f2fa20931dd009cd3aa311f8e1366f67
Marvin Hofmann <marvin@marv-productions.de>
2017-05-20 16:19:40 +0200

    add redux

--- sosml-web/src/components/Hello.tsx
+++ sosml-web/src/components/Hello.tsx
@@ -4,9 +4,11 @@ import "./Hello.css";
 export interface Props {
     name: string;
     enthusiasmLevel?: number;
+    onIncrement?: () => void;
+    onDecrement?: () => void;
 }
 
-function Hello({name, enthusiasmLevel = 1}: Props) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
     if (enthusiasmLevel <= 0) {
         throw new Error('You could be a little more enthusiastic. :D');
     }
@@ -15,6 +17,10 @@ function Hello({name, enthusiasmLevel = 1}: Props) {
         <div className="hello">
             <div className="greeting">
                 Hello {name + getExclamationMarks(enthusiasmLevel)}
+            </div>
+            <div>
+                <button onClick={onDecrement}>-</button>
+                <button onClick={onIncrement}>+</button>
             </div>
         </div>
     );
--- sosml-web/src/index.tsx
+++ sosml-web/src/index.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "./index.css";
-import Hello from "./components/Hello";
+import Hello from "./containers/Hello";
+import {Provider} from "react-redux";
+
+import {createStore} from "redux";
+import {enthusiasm} from "./reducers/index";
+import {StoreState} from "./types/index";
+
+const store = createStore<StoreState>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-    <Hello name="TypeScript" enthusiasmLevel={10}/>,
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
     document.getElementById('root') as HTMLElement
 );
