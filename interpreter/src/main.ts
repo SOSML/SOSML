bab8d07dd1ea3a912b133468866503c83d7f415e
Philip Wellnitz <philipwellnitz@gmx.de>
2017-06-02 16:19:31 +0200

    bfs, close #10

--- interpreter/src/main.ts
+++ interpreter/src/main.ts
@@ -13,8 +13,7 @@ let AST = instance.lexParse(..code..);
 
 */
 
-import State        = require('./state');
-import * as Lexer from "./lexer";
+//import * as Lexer from "./lexer";
 // . . .
 
 /*
@@ -23,14 +22,13 @@ import * as Lexer from "./lexer";
  *      - value (string, int, tuple, etc)
  *      - type (SML type)
  */
-export type State = Map<string,(int, any, any)>;
+export type State = Map<string,[number, any, any]>;
 
 export class API {
     /* Think of some additional flags n stuff etc */
     static interpret(oldState : State, nextInstruction : string) : State {
         // TODO
-        Lexer lexer = Lexer();
-        tkn = lexer.lex(nextInstruction);
+        //let tkn = Lexer.lex(nextInstruction);
 
         // Parser parser = Parser();
         // ast = parser.parse(tkn);
@@ -42,12 +40,12 @@ export class API {
         // let newState = interpreter.interpret(oldState, ast);
 
         // return newState;
-        return State;
+        return oldState;
     }
 
-    static interpret(oldState : State, currentPartialInstruction : string, nextInstructionPart : string) : (State,string) | Error {
+    static interpretFurther(oldState : State, currentPartialInstruction : string, nextInstructionPart : string) : [State,string] | Error {
         try {
-            return (interpret(oldState, currentPartialInstruction + nextInstructionPart), "");
+            return [API.interpret(oldState, currentPartialInstruction + nextInstructionPart), ""];
         } catch( e ) {
             //if( e instanceof IncompleteCode ) {
             //  return (oldState, currentPartialInstruction + nextInstructionPart);
