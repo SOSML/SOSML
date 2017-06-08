a35345d39094cefc3be4d014bc6d49a3d5e74fe0
Jujuedv <jujuedv@gmail.com>
2017-06-08 23:35:17 +0200

    lexer tests: incorporated position

--- interpreter/test/lexer.ts
+++ interpreter/test/lexer.ts
@@ -235,7 +235,7 @@ it("dots", () => {
     expect(() => { API.lex(testcase2); }).toThrow(API.LexerError);
 
     expect(API.lex(testcase3)).toEqual([
-        new API.KeywordToken("...")
+        new API.KeywordToken("...", 0)
     ]);
 });
 
@@ -243,54 +243,54 @@ it("reserved words", () => {
     let testcase: string = 'abstype and andalso as case datatype do else end exception fn fun handle if in infix infixr let local nonfix of op open orelse raise rec then type val with withtype while ( ) [ ] { } , : ; ... _ | = => -> #';
 
     expect(API.lex(testcase)).toEqual([
-        new API.KeywordToken("abstype"),
-        new API.KeywordToken("and"),
-        new API.KeywordToken("andalso"),
-        new API.KeywordToken("as"),
-        new API.KeywordToken("case"),
-        new API.KeywordToken("datatype"),
-        new API.KeywordToken("do"),
-        new API.KeywordToken("else"),
-        new API.KeywordToken("end"),
-        new API.KeywordToken("exception"),
-        new API.KeywordToken("fn"),
-        new API.KeywordToken("fun"),
-        new API.KeywordToken("handle"),
-        new API.KeywordToken("if"),
-        new API.KeywordToken("in"),
-        new API.KeywordToken("infix"),
-        new API.KeywordToken("infixr"),
-        new API.KeywordToken("let"),
-        new API.KeywordToken("local"),
-        new API.KeywordToken("nonfix"),
-        new API.KeywordToken("of"),
-        new API.KeywordToken("op"),
-        new API.KeywordToken("open"),
-        new API.KeywordToken("orelse"),
-        new API.KeywordToken("raise"),
-        new API.KeywordToken("rec"),
-        new API.KeywordToken("then"),
-        new API.KeywordToken("type"),
-        new API.KeywordToken("val"),
-        new API.KeywordToken("with"),
-        new API.KeywordToken("withtype"),
-        new API.KeywordToken("while"),
-        new API.KeywordToken("("),
-        new API.KeywordToken(")"),
-        new API.KeywordToken("["),
-        new API.KeywordToken("]"),
-        new API.KeywordToken("{"),
-        new API.KeywordToken("}"),
-        new API.KeywordToken(","),
-        new API.KeywordToken(":"),
-        new API.KeywordToken(";"),
-        new API.KeywordToken("..."),
-        new API.KeywordToken("_"),
-        new API.KeywordToken("|"),
-        new API.EqualsToken(),
-        new API.KeywordToken("=>"),
-        new API.KeywordToken("->"),
-        new API.KeywordToken("#")
+        new API.KeywordToken("abstype", 0),
+        new API.KeywordToken("and", 8),
+        new API.KeywordToken("andalso", 12),
+        new API.KeywordToken("as", 20),
+        new API.KeywordToken("case", 23),
+        new API.KeywordToken("datatype", 28),
+        new API.KeywordToken("do", 37),
+        new API.KeywordToken("else", 40),
+        new API.KeywordToken("end", 45),
+        new API.KeywordToken("exception", 49),
+        new API.KeywordToken("fn", 59),
+        new API.KeywordToken("fun", 62),
+        new API.KeywordToken("handle", 66),
+        new API.KeywordToken("if", 73),
+        new API.KeywordToken("in", 76),
+        new API.KeywordToken("infix", 79),
+        new API.KeywordToken("infixr", 85),
+        new API.KeywordToken("let", 92),
+        new API.KeywordToken("local", 96),
+        new API.KeywordToken("nonfix", 102),
+        new API.KeywordToken("of", 109),
+        new API.KeywordToken("op", 112),
+        new API.KeywordToken("open", 115),
+        new API.KeywordToken("orelse", 120),
+        new API.KeywordToken("raise", 127),
+        new API.KeywordToken("rec", 133),
+        new API.KeywordToken("then", 137),
+        new API.KeywordToken("type", 142),
+        new API.KeywordToken("val", 147),
+        new API.KeywordToken("with",151),
+        new API.KeywordToken("withtype", 156),
+        new API.KeywordToken("while", 165),
+        new API.KeywordToken("(", 171),
+        new API.KeywordToken(")", 173),
+        new API.KeywordToken("[", 175),
+        new API.KeywordToken("]", 177),
+        new API.KeywordToken("{", 179),
+        new API.KeywordToken("}", 181),
+        new API.KeywordToken(",", 183),
+        new API.KeywordToken(":", 185),
+        new API.KeywordToken(";", 187),
+        new API.KeywordToken("...", 189),
+        new API.KeywordToken("_", 193),
+        new API.KeywordToken("|", 195),
+        new API.EqualsToken(197),
+        new API.KeywordToken("=>", 199),
+        new API.KeywordToken("->", 202),
+        new API.KeywordToken("#", 205)
     ])
 });
 
@@ -303,22 +303,22 @@ it("integer constants decimal", () => {
     let testcase_bigzero: string = '000000';
 
     expect(API.lex(testcase_nonint)).toEqual([
-        new API.IdentifierToken("~")
+        new API.IdentifierToken("~", 0)
     ]);
     expect(API.lex(testcase_pos)).toEqual([
-        new API.NumericToken(testcase_pos, 42)
+        new API.NumericToken(testcase_pos, 0, 42)
     ]);
     expect(API.lex(testcase_neg)).toEqual([
-        new API.IntegerConstantToken(testcase_neg, -69)
+        new API.IntegerConstantToken(testcase_neg, 0, -69)
     ]);
     expect(API.lex(testcase_pos_leadingzero)).toEqual([
-        new API.IntegerConstantToken(testcase_pos_leadingzero, 42)
+        new API.IntegerConstantToken(testcase_pos_leadingzero, 0, 42)
     ]);
     expect(API.lex(testcase_neg_leadingzero)).toEqual([
-        new API.IntegerConstantToken(testcase_neg_leadingzero, -23)
+        new API.IntegerConstantToken(testcase_neg_leadingzero, 0, -23)
     ]);
     expect(API.lex(testcase_bigzero)).toEqual([
-        new API.IntegerConstantToken(testcase_bigzero, 0)
+        new API.IntegerConstantToken(testcase_bigzero, 0, 0)
     ]);
 });
 
@@ -333,28 +333,28 @@ it("integer constants hexadecimal wellformed", () => {
     let testcase_all_chars3: string = '0xABCDEF';
 
     expect(API.lex(testcase_pos)).toEqual([
-        new API.NumericToken(testcase_pos, 0x4a)
+        new API.NumericToken(testcase_pos, 0, 0x4a)
     ]);
     expect(API.lex(testcase_neg)).toEqual([
-        new API.IntegerConstantToken(testcase_neg, -0x6e)
+        new API.IntegerConstantToken(testcase_neg, 0, -0x6e)
     ]);
     expect(API.lex(testcase_pos_leadingzero)).toEqual([
-        new API.IntegerConstantToken(testcase_pos_leadingzero, 0x4f)
+        new API.IntegerConstantToken(testcase_pos_leadingzero, 0, 0x4f)
     ]);
     expect(API.lex(testcase_neg_leadingzero)).toEqual([
-        new API.IntegerConstantToken(testcase_neg_leadingzero, -0xa)
+        new API.IntegerConstantToken(testcase_neg_leadingzero, 0, -0xa)
     ]);
     expect(API.lex(testcase_bigzero)).toEqual([
-        new API.IntegerConstantToken(testcase_bigzero, 0)
+        new API.IntegerConstantToken(testcase_bigzero, 0, 0)
     ]);
     expect(API.lex(testcase_all_chars1)).toEqual([
-        new API.IntegerConstantToken(testcase_all_chars1, 0x123456789)
+        new API.IntegerConstantToken(testcase_all_chars1, 0, 0x123456789)
     ]);
     expect(API.lex(testcase_all_chars2)).toEqual([
-        new API.IntegerConstantToken(testcase_all_chars2, 0xabcdef)
+        new API.IntegerConstantToken(testcase_all_chars2, 0, 0xabcdef)
     ]);
     expect(API.lex(testcase_all_chars3)).toEqual([
-        new API.IntegerConstantToken(testcase_all_chars3, 0xabcdef)
+        new API.IntegerConstantToken(testcase_all_chars3, 0, 0xabcdef)
     ]);
 });
 
@@ -369,35 +369,35 @@ it("integer constants hexadecimal illformed", () => {
     let testcase_capital_x: string = '0X4a';
 
     expect(API.lex(testcase_nonint)).toEqual([
-        new API.IntegerConstantToken("~0", -0),
-        new API.IdentifierToken("x")
+        new API.IntegerConstantToken("~0", 0, -0),
+        new API.AlphanumericIdentifierToken("x", 2)
     ]);
     expect(API.lex(testcase_too_long_prefix)).toEqual([
-        new API.IntegerConstantToken("00", 0),
-        new API.IdentifierToken("x42")
+        new API.IntegerConstantToken("00", 0, 0),
+        new API.AlphanumericIdentifierToken("x42", 2)
     ]);
     expect(API.lex(testcase_too_short_prefix)).toEqual([
-        new API.IdentifierToken("x42")
+        new API.AlphanumericIdentifierToken("x42", 0)
     ]);
     expect(API.lex(testcase_neg_too_long_prefix)).toEqual([
-        new API.IntegerConstantToken("~00", -0),
-        new API.IdentifierToken("x69")
+        new API.IntegerConstantToken("~00", 0, -0),
+        new API.AlphanumericIdentifierToken("x69", 3)
     ]);
     expect(API.lex(testcase_neg_too_short_prefix)).toEqual([
-        new API.IdentifierToken("~"),
-        new API.IdentifierToken("x42")
+        new API.IdentifierToken("~", 0),
+        new API.AlphanumericIdentifierToken("x42", 1)
     ]);
     expect(API.lex(testcase_not_hex)).toEqual([
-        new API.IntegerConstantToken("0xabc", 0xabc),
-        new API.IdentifierToken("gcba")
+        new API.IntegerConstantToken("0xabc", 0, 0xabc),
+        new API.AlphanumericIdentifierToken("gcba", 5)
     ]);
     expect(API.lex(testcase_missing_x)).toEqual([
-        new API.IntegerConstantToken("04", 4),
-        new API.IdentifierToken("a")
+        new API.IntegerConstantToken("04", 0, 4),
+        new API.AlphanumericIdentifierToken("a", 2)
     ]);
     expect(API.lex(testcase_capital_x)).toEqual([
-        new API.IntegerConstantToken("0", 0),
-        new API.IdentifierToken("X4a")
+        new API.IntegerConstantToken("0", 0, 0),
+        new API.AlphanumericIdentifierToken("X4a", 1)
     ]);
 });
 
@@ -411,29 +411,29 @@ it("word constants decimal", () => {
     let testcase_neg: string = '~0w69';
 
     expect(API.lex(testcase_noword)).toEqual([
-        new API.IntegerConstantToken("0", 0),
-        new API.IdentifierToken("w")
+        new API.IntegerConstantToken("0", 0, 0),
+        new API.AlphanumericIdentifierToken("w", 1)
     ]);
     expect(API.lex(testcase_pos)).toEqual([
-        new API.WordConstantToken("0w42", 42)
+        new API.WordConstantToken("0w42", 0, 42)
     ]);
     expect(API.lex(testcase_nohex)).toEqual([
-        new API.IntegerConstantToken("0", 0),
-        new API.IdentifierToken("w9a")
+        new API.WordConstantToken("0w9", 0, 9),
+        new API.AlphanumericIdentifierToken("a", 3)
     ]);
     expect(API.lex(testcase_capital_w)).toEqual([
-        new API.IntegerConstantToken("0", 0),
-        new API.IdentifierToken("W1337")
+        new API.IntegerConstantToken("0", 0, 0),
+        new API.AlphanumericIdentifierToken("W1337", 1)
     ]);
     expect(API.lex(testcase_zero_after_w)).toEqual([
-        new API.WordConstantToken("0w01337", 1337)
+        new API.WordConstantToken("0w01337", 0, 1337)
     ]);
     expect(API.lex(testcase_leading_zero)).toEqual([
-        new API.IntegerConstantToken("00", 0),
-        new API.IdentifierToken("w01")
+        new API.IntegerConstantToken("00", 0, 0),
+        new API.AlphanumericIdentifierToken("w01", 2)
     ]);
-    expect(API.lex(testcase_leading_zero)).toEqual([
-        new API.IntegerConstantToken("~0", -0),
-        new API.IdentifierToken("w69")
+    expect(API.lex(testcase_neg)).toEqual([
+        new API.IntegerConstantToken("~0", 0, -0),
+        new API.AlphanumericIdentifierToken("w69", 2)
     ]);
 });
