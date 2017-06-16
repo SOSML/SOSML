02179888d743d1e38f79c800224f1834cfb50745
Marvin Hofmann <marvin@marv-productions.de>
2017-06-16 14:34:53 +0200

    eu

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const cmd = require('node-cmd');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
@@ -9,11 +10,20 @@ server.route({
     method: ['GET', 'POST'],
     path: '/',
     handler: function (request, reply) {
-        var payload = request.payload
+        var payload = request.payload;
 
-        console.log(payload)
+        console.log(payload);
 
-        reply(payload)
+        const ret = new Promise(function (resolve, reject) {
+            cmd.get(
+                'pwd',
+                function(err, data, stderr){
+                    resolve('the current working dir is : '+ data);
+                }
+            );
+        });
+
+        reply(ret).code(200);
     }
 });
 
