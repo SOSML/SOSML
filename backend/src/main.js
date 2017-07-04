70f0cf50e8e5acc9e927a3bcc837d334a6edf0b0
Marvin Hofmann <marvin@marv-productions.de>
2017-07-04 21:29:57 +0200

    add sharing v1

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,13 +4,22 @@ const express = require('express');
 const cmd = require('node-cmd');
 const bodyparser = require('body-parser');
 const path = require('path');
+const helmet = require('helmet');
+const compression = require('compression');
+const fs = require('fs');
+const crypto = require('crypto');
+
 
 const server = express();
+server.use(helmet())
+server.use(compression())
 server.use(bodyparser.json());
 server.use('/static/', express.static('frontend/static'));
+server.use('/share/', express.static(path.join(__dirname, 'shares'));
 server.get('/', function (request, response) {
     response.sendFile(path.resolve(__dirname+'/../frontend/index.html'));
 });
+
 
 server.post('/api/fallback/',
     function (request, response) {
@@ -24,6 +33,21 @@ server.post('/api/fallback/',
             }
         );
         dockerrunner.stdin.write(payload);
+    }
+);
+
+server.put('/api/share/',
+    function (request, response) {
+        var payload = request.body.code;
+        const hash = crypto.createHash('md5').update(payload).digest("base64");
+        fs.writeFile("./shares/"+hash+".sml", payload, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+            response.set('Content-Type', 'text/plain');
+            response.end(hash);
+        });
     }
 );
 
