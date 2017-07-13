a39a6d9fbeb36ed3ad6d235d219a709abf79b2a9
Marvin Hofmann <marvin@marv-productions.de>
2017-07-13 10:07:09 +0200

    some improvements to the api

--- backend/src/main.js
+++ backend/src/main.js
@@ -15,7 +15,8 @@ server.use(helmet())
 server.use(compression())
 server.use(bodyparser.json());
 server.use('/static/', express.static('../frontend/build/static'));
-server.use('/share/', express.static(path.join(__dirname, 'shares')));
+// server.use('/share/', express.static('shares'));
+// server.use('/code/', express.static('code'));
 server.get('/', function (request, response) {
     response.sendFile(path.resolve('../frontend/build/index.html'));
 });
@@ -28,6 +29,17 @@ server.post('/api/fallback/',
         let dockerrunner = cmd.get(
             'docker run --cpus=1 --memory=128m --rm -i --read-only derjesko/mosmlfallback',
             function(err, data, stderr){
+                var last_line = data.split(/\r?\n/).pop();
+                var error_code = parseInt(last_line.substring(2));
+                error_text = '';
+                if(error_code > 0){
+                    if(error_code == 124){
+                        error_text = 'SML hit the time limit of 3 seconds.';
+                    }else {
+                        data = data.replace(last_line, 'SML exited with ' + error_code);
+                    }
+                }
+                data = data.replace(last_line, error_text);
                 response.set('Content-Type', 'text/plain');
                 response.end(data);
             }
@@ -51,4 +63,39 @@ server.put('/api/share/',
     }
 );
 
-server.listen(80, () => {console.log('yay');} )
+server.get('/share/:code',
+    function (request, response) {
+        var code = request.params.code;
+        fs.readFile("./shares/"+code+".sml", 'utf8', function(err, data) {
+            if(err) {
+                return console.log(err);
+            }
+            response.set('Content-Type', 'text/plain');
+            response.end(data);
+        });
+    }
+);
+
+server.get('/api/list/',
+    function (request, response) {
+        fs.readdir('./code/', function(err, items) {
+            response.set('Content-Type', 'text/json');
+            response.end(JSON.stringify({codes: items}));
+        });
+    }
+);
+
+server.get('/code/:code',
+    function (request, response) {
+        var code = request.params.code;
+        fs.readFile("./code/"+code, 'utf8', function(err, data) {
+            if(err) {
+                return console.log(err);
+            }
+            response.set('Content-Type', 'text/plain');
+            response.end(data);
+        });
+    }
+);
+
+server.listen(80, () => {console.log('yay');} );
