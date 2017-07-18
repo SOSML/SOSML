12ffd198d21885c3cdb8a145c621dc148ef24064
jesko <s8jedujm@stud.uni-saarland.de>
2017-07-18 23:11:20 +0200

    added more simplify tests

--- interpreter/test/simplify.ts
+++ interpreter/test/simplify.ts
@@ -26,17 +26,29 @@ function pattern_tester(pattern: Expr.Pattern, pos42: Errors.Position): Decl.Dec
     ]);
 }
 
+function expression_tester(expression: Expr.Expression) {
+    return new Decl.SequentialDeclaration(
+        0,
+        [
+            new Decl.ValueDeclaration(
+                0,
+                [],
+                [
+                    new Decl.ValueBinding(
+                        4,
+                        false,
+                        new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
+                        expression
+                    )
+                ]
+            )
+        ]
+    )
+}
+
 it("exp - tuple to record", () => {
     expect(parse("val x = ();").simplify()).toEqualWithType(
-        new Decl.SequentialDeclaration(0,[
-            new Decl.ValueDeclaration(0, [], [
-                new Decl.ValueBinding(
-                    4, false,
-                    new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
-                    new Expr.Record(8, true, [])
-                )
-            ])
-        ])
+        expression_tester(new Expr.Record(8, true, []));
     );
 
     let record: Expr.Record = new Expr.Record(8, true, [
@@ -44,45 +56,21 @@ it("exp - tuple to record", () => {
         ["2", new Expr.Constant(13, new Lexer.NumericToken("12", 13, 12))]
     ]);
     expect(parse("val x = (11, 12);").simplify()).toEqualWithType(
-        new Decl.SequentialDeclaration(0,[
-            new Decl.ValueDeclaration(0, [], [
-                new Decl.ValueBinding(
-                    4, false,
-                    new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
-                    record;
-                )
-            ])
-        ])
+        expression_tester(record);
     );
 
-    let record: Expr.Record = new Expr.Record(8, true, [
+    let record1: Expr.Record = new Expr.Record(8, true, [
         ["1", new Expr.Constant(9, new Lexer.NumericToken("11", 9, 11))],
         ["2", new Expr.Constant(13, new Lexer.NumericToken("12", 13, 12))],
         ["3", new Expr.ValueIdentifier(17, new Lexer.AlphanumericIdentifierToken("a", 17))],
         ["4", new Expr.Constant(20, new Lexer.NumericToken("14", 20, 14))],
     ]);
     expect(parse("val x = (11, 12, a, 14);").simplify()).toEqualWithType(
-        new Decl.SequentialDeclaration(0,[
-            new Decl.ValueDeclaration(0, [], [
-                new Decl.ValueBinding(
-                    4, false,
-                    new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
-                    record;
-                )
-            ])
-        ])
+        expression_tester(record1);
     );
 
     expect(parse("val x = (11);").simplify()).toEqualWithType(
-        new Decl.SequentialDeclaration(0,[
-            new Decl.ValueDeclaration(0, [], [
-                new Decl.ValueBinding(
-                    4, false,
-                    new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
-                    new Expr.Constant(9, new Lexer.NumericToken("11", 9, 11));
-                )
-            ])
-        ])
+        expression_tester(new Expr.Constant(9, new Lexer.NumericToken("11", 9, 11)))
     )
 });
 
@@ -103,15 +91,7 @@ it("exp", () => {
     );
     let try_this: string = "val x = fn {y = __rs, ...} => __rs;";
     expect(parse(try_this).simplify()).toEqualWithType(
-        new Decl.SequentialDeclaration(0,[
-            new Decl.ValueDeclaration(0, [], [
-                new Decl.ValueBinding(
-                    4, false,
-                    new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
-                    lambda
-                )
-            ])
-        ])
+        expression_tester(lambda)
     );
 
     let lambda1: Expr.Lambda = new Expr.Lambda(
@@ -129,16 +109,59 @@ it("exp", () => {
         )
     );
     expect(parse("val x = # y;").simplify()).toEqualWithType(
-        new Decl.SequentialDeclaration(0,[
-            new Decl.ValueDeclaration(0, [], [
-                new Decl.ValueBinding(
-                    4, false,
-                    new Expr.ValueIdentifier(4, new Lexer.AlphanumericIdentifierToken("x", 4)),
-                    lambda1
-                )
-            ])
-        ])
+        expression_tester(lambda1)
     );
+
+    let case_stuff: Expr.Expression = new Expr.FunctionApplication(
+        8,
+        new Expr.Lambda(
+            8,
+            new Expr.Match(
+                19,
+                [[new Expr.Wildcard(19), new Expr.Constant(24, new Lexer.NumericToken("1", 24, 1))]]
+            )
+        ),
+        new Expr.Constant(13, new Lexer.NumericToken("42", 13, 42))
+    );
+    expect(parse("val x = case 42 of _ => 1;").simplify()).toEqualWithType(
+        expression_tester(case_stuff)
+    );
+
+    let if_then_else: Expr.Expression = new Expr.FunctionApplication(
+        8,
+        new Expr.Lambda(8, new Expr.Match(8, [
+            [new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("true", 0)), new Expr.ValueIdentifier(21, new Lexer.AlphanumericIdentifierToken("blah", 21))],
+            [new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("false", 0)), new Expr.ValueIdentifier(31, new Lexer.AlphanumericIdentifierToken("blub", 31))]
+        ])),
+        new Expr.ValueIdentifier(11, new Lexer.AlphanumericIdentifierToken("fish", 11))
+    )
+    expect(parse("val x = if fish then blah else blub;").simplify()).toEqualWithType(
+        expression_tester(if_then_else)
+    )
+
+    let orelse: Expr.Expression = new Expr.FunctionApplication(
+        8,
+        new Expr.Lambda(8, new Expr.Match(8, [
+            [new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("true", 0)), new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("true", 0))],
+            [new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("false", 0)), new Expr.ValueIdentifier(20, new Lexer.AlphanumericIdentifierToken("blub", 20))]
+        ])),
+        new Expr.ValueIdentifier(8, new Lexer.AlphanumericIdentifierToken("fish", 8))
+    )
+    expect(parse("val x = fish orelse blub;").simplify()).toEqualWithType(
+        expression_tester(orelse)
+    )
+
+    let andalso: Expr.Expression = new Expr.FunctionApplication(
+        8,
+        new Expr.Lambda(8, new Expr.Match(8, [
+            [new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("true", 0)), new Expr.ValueIdentifier(21, new Lexer.AlphanumericIdentifierToken("blah", 21))],
+            [new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("false", 0)), new Expr.ValueIdentifier(0, new Lexer.IdentifierToken("false", 0))]
+        ])),
+        new Expr.ValueIdentifier(8, new Lexer.AlphanumericIdentifierToken("fish", 8))
+    )
+    expect(parse("val x = fish andalso blah;").simplify()).toEqualWithType(
+        expression_tester(andalso)
+    )
     //TODO
 });
 
