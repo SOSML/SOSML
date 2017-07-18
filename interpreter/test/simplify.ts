dc0a73b92bf340371a2bc2f98528f95be3c3ed62
jesko <s8jedujm@stud.uni-saarland.de>
2017-07-18 08:25:28 +0200

    more tests

--- interpreter/test/simplify.ts
+++ interpreter/test/simplify.ts
@@ -35,6 +35,16 @@ it("fvalbind", () => {
 });
 
 it("dec", () => {
+    let pattern: Expr.Expression = new Expr.ValueIdentifier(-1, new Lexer.AlphanumericIdentifierToken("it",-1));
+    let pattern2: Expr.Expression = new Expr.ValueIdentifier(8, new Lexer.AlphanumericIdentifierToken("x",8));
+    expect(parse("fun f x = x ; val x = 42;")).toEqualWithType(new Decl.SequentialDeclaration(0, [
+        new Decl.ValueDeclaration(0,[], [
+            new Decl.ValueBinding(0, false, pattern, new Expr.Constant(0, new Lexer.NumericToken("10", 0, 10)))
+        ]),
+        new Decl.ValueDeclaration(4,[], [
+            new Decl.ValueBinding(8, false, pattern2, new Expr.Constant(12, new Lexer.NumericToken("42", 12, 42)))
+        ])
+    ]))
     //TODO
 });
 
@@ -51,5 +61,17 @@ it("funbind", () => {
 });
 
 it("program", () => {
+    //val it = 10;
+    let pattern: Expr.Expression = new Expr.ValueIdentifier(-1, new Lexer.AlphanumericIdentifierToken("it",-1));
+    let pattern2: Expr.Expression = new Expr.ValueIdentifier(8, new Lexer.AlphanumericIdentifierToken("x",8));
+    expect(parse("10; val x = 42;")).toEqualWithType(new Decl.SequentialDeclaration(0, [
+        new Decl.ValueDeclaration(0,[], [
+            new Decl.ValueBinding(0, false, pattern, new Expr.Constant(0, new Lexer.NumericToken("10", 0, 10)))
+        ]),
+        new Decl.ValueDeclaration(4,[], [
+            new Decl.ValueBinding(8, false, pattern2, new Expr.Constant(12, new Lexer.NumericToken("42", 12, 42)))
+        ])
+    ]))
+
     //TODO
 });
--- interpreter/test/test_helper.ts
+++ interpreter/test/test_helper.ts
@@ -13,6 +13,35 @@ const NO_DIFF_MESSAGE = chalk.dim(
 
 export function init(): void {
     expect.extend({
+      toEqualWithTypeNoPosition(received, expected) {
+        
+        // we assume that diff prints the types as well, so if there is no diff, we assume that recieved == expected
+        const diffString = diff(expected, received, {
+          expand: this.expand
+        });
+
+        const pass = diffString == NO_DIFF_MESSAGE;
+
+        const message = pass
+          ? () => this.utils.matcherHint('.not.toEqualWithTypeNoPosition') + '\n\n' +
+            `Expected value to not equal with type:\n` +
+            `  ${this.utils.printExpected(expected)}\n` +
+            `Received:\n` +
+            `  ${this.utils.printReceived(received)}`
+          : () => {
+            return this.utils.matcherHint('.toEqualWithTypeNoPosition') + '\n\n' +
+            `Expected value to equal with type:\n` +
+            `  ${this.utils.printExpected(expected)}\n` +
+            `Received:\n` +
+            `  ${this.utils.printReceived(received)}` +
+            (diffString ? `\n\nDifference:\n\n${diffString}` : '');
+          };
+
+        return {actual: received, message, pass};
+      },
+    });
+
+    expect.extend({
       toEqualWithType(received, expected) {
         // we assume that diff prints the types as well, so if there is no diff, we assume that recieved == expected
         const diffString = diff(expected, received, {
