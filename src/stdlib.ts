27497ce041b2d9bbf29959aee689e9b33b5016d9
Philip Wellnitz <philipwellnitz@gmx.de>
2017-11-21 11:49:40 +0100

    wc

--- src/stdlib.ts
+++ src/stdlib.ts
@@ -11,7 +11,7 @@ let intType = new CustomType('int');
 let realType = new CustomType('real');
 // let wordType = new CustomType('word');
 // let boolType = new CustomType('bool');
-// let stringType = new CustomType('string');
+let stringType = new CustomType('string');
 let charType = new CustomType('char');
 
 
@@ -360,6 +360,12 @@ export let STDLIB: {
                     if #"a" <= c andalso c <= #"z" then chr (ord c - 32)
                     else c;
 
+                fun compare (a, b) = Int.compare(ord a, ord b);
+                fun op< (a, b) = Int.compare(ord a, ord b) = LESS;
+                fun op> (a, b) = Int.compare(ord a, ord b) = GREATER;
+                fun op<= (a, b) = Int.compare(ord a, ord b) <> GREATER;
+                fun op>= (a, b) = Int.compare(ord a, ord b) <> LESS;
+
                 val ord = ord;
                 val chr = chr;
             end;`,
@@ -613,6 +619,63 @@ export let STDLIB: {
                 fun compare (x, y: real) = if x < y then LESS else if x > y then GREATER else EQUAL;
             end;`,
         'requires': undefined },
+    'String': {
+        'native': undefined,
+        'code': `structure String : sig
+            eqtype string
+            eqtype char
+            val maxSize : int
+            val size : string -> int
+            val sub : string * int -> char
+            val extract   : string * int * int option -> string
+            val substring : string * int * int -> string
+            val ^ : string * string -> string
+            val concat : string list -> string
+            val concatWith : string -> string list -> string
+            val str : char -> string
+            val implode : char list -> string
+            val explode : string -> char list
+            val map : (char -> char) -> string -> string
+            val translate : (char -> string) -> string -> string
+            val tokens : (char -> bool) -> string -> string list
+            val fields : (char -> bool) -> string -> string list
+            val isPrefix    : string -> string -> bool
+            val isSubstring : string -> string -> bool
+            val isSuffix    : string -> string -> bool
+            val compare : string * string -> order
+            val collate : (char * char -> order)
+                            -> string * string -> order
+            val <  : string * string -> bool
+            val <= : string * string -> bool
+            val >  : string * string -> bool
+            val >= : string * string -> bool
+
+            val toString : string -> String.string
+            val scan       : (char, 'a) StringCvt.reader
+                               -> (string, 'a) StringCvt.reader
+            val fromString : String.string -> string option
+            val toCString : string -> String.string
+            val fromCString : String.string -> string option
+
+            end = struct
+                type string = string;
+                type char = char;
+
+                fun cmp ([], []) = EQUAL
+                  | cmp ([], _) = LESS
+                  | cmp (_, []) = GREATER
+                  | cmp (x::xs, y::ys) = let
+                        val tmp = Char.compare (x, y);
+                    in
+                        if tmp <> EQUAL then tmp else cmp (xs, ys)
+                    end;
+
+                fun collate (a, b) = cmp (explode a, explode b);
+                val implode = implode;
+                val explode = explode;
+            end;`,
+        'requires': ['Char']
+    },
     'Version': {
         'native': undefined,
         'code': `structure Version = struct
